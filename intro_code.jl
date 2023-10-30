f8efd1a7d81772ec72ac625eeccbd0dc47c2da33
Yusheng Zhao <yushengzhao2020@outlook.com>
2023-10-30 15:31:10 +0800

    update code

--- intro_code.jl
+++ intro_code.jl
@@ -2,28 +2,30 @@ using Pkg;
 Pkg.activate(".")
 using JuMP, Random, GLPK
 
-order = 5
-num_points = 1000
-x_rand = rand(Float64,num_points) .* 2.0 .- 1.0
+function guess_slope(order::Integer, num_points::Integer)
 
-model = Model()
-set_optimizer(model, GLPK.Optimizer)
+    x_rand = rand(Float64, num_points) .* 2.0 .- 1.0
 
+    model = Model()
+    set_optimizer(model, GLPK.Optimizer)
 
-#Define variables
-@variable(model, coeffs[i=1:order] )
+    #Define variables
+    @variable(model, coeffs[i = 1:order])
 
-#Define Constraints
-for x_pt in x_rand
-    constr_expr = sum([coeffs[i] * x_pt^(i-1) for i in 1:order])
-    @constraint(model, constr_expr <= 1)
-    @constraint(model, constr_expr >= -1)
+    #Define Constraints
+    for x_pt in x_rand
+        constr_expr = sum([coeffs[i] * x_pt^(i - 1) for i = 1:order])
+        @constraint(model, constr_expr <= 1)
+        @constraint(model, constr_expr >= -1)
+    end
+
+    #Define Objective
+    @objective(model, Max, coeffs[2])
+
+    #Run the opimization
+    optimize!(model)
+
+    println("The guessed maximal slope is: ", objective_value(model))
 end
 
-#Define Objective
-@objective(model, Max, coeffs[2])
-
-#Run the opimization
-optimize!(model)
-
-objective_value(model)
+guess_slope(parse(Int, ARGS[1]), parse(Int, ARGS[2]))
